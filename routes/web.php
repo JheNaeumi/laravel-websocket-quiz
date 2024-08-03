ab9dbb39b492fb051121e3b1e15bca669edb67d9
Jhe <jeremiahpasion56@gmail.com>
2024-08-03 23:03:06 +0800

    add quiz route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\QuizController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -12,6 +13,10 @@ Route::get('/', function () {
         'laravelVersion' => Application::VERSION,
         'phpVersion' => PHP_VERSION,
     ]);
+});
+Route::middleware(['auth'])->group(function () {
+    Route::get('/quiz', [QuizController::class, 'index']);
+    Route::post('/start-quiz', [QuizController::class, 'startQuiz']);
 });
 
 Route::get('/dashboard', function () {
