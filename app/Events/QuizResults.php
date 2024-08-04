4d228ff4dcc5baa9898bee8a12b3710386cd03c2
Jhe <jeremiahpasion56@gmail.com>
2024-08-04 23:04:15 +0800

    add quizResultEvent

---
+++ app/Events/QuizResults.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class QuizResults implements ShouldBroadcast
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $results;
+    public $lobbyId;
+
+    public function __construct($results, $lobbyId)
+    {
+        $this->results = $results;
+        $this->lobbyId = $lobbyId;
+    }
+
+    public function broadcastOn()
+    {
+        return new Channel('lobby.' . $this->lobbyId);
+    }
+}
