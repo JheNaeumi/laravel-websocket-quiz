92fb6e87c868b15fae838f3edfd000ddcd4dafa4
Jhe <jeremiahpasion56@gmail.com>
2024-08-04 23:05:48 +0800

    add lobby & question model

---
+++ app/Models/Lobby.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Lobby extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['name', 'host_id', 'is_active'];
+
+    public function host()
+    {
+        return $this->belongsTo(User::class, 'host_id');
+    }
+
+    public function participants()
+    {
+        return $this->belongsToMany(User::class, 'lobby_user')->withTimestamps();
+    }
+}
