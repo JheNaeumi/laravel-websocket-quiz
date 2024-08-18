e413f5d0b428ee1fc153489a6c9898b6e926a393
Jhe <jeremiahpasion56@gmail.com>
2024-08-18 22:13:14 +0800

    update participantanswer event

--- app/Events/ParticipantAnswered.php
+++ app/Events/ParticipantAnswered.php
@@ -18,16 +18,35 @@ class ParticipantAnswered implements ShouldBroadcast
     public $quiz;
     public $participantName;
     public $answer;
+    public $questionIndex;  // Add this if you want to track which question was answered
 
-    public function __construct(Quiz $quiz, $participantName, $answer)
+    public function __construct(Quiz $quiz, $participantName, $answer, $questionIndex)
     {
         $this->quiz = $quiz;
         $this->participantName = $participantName;
         $this->answer = $answer;
+        $this->questionIndex = $questionIndex;
     }
 
-    public function broadcastOn() : array
+    public function broadcastOn(): array
     {
-        return [new Channel("quizzes.{$this->quiz->id}"),];
+        return [
+            new Channel("quizzes.{$this->quiz->id}"),
+        ];
+    }
+
+    // public function broadcastAs(): string
+    // {
+    //     return 'participant.answered';
+    // }
+
+    public function broadcastWith(): array
+    {
+        return [
+            'quiz_id' => $this->quiz->id,
+            'participant_name' => $this->participantName,
+            'answer' => $this->answer,
+            'question_index' => $this->questionIndex,
+        ];
     }
 }
