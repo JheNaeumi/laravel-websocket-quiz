d13af0c1f4acfd5326c3415e19d9b6ff929f5e21
Jhe <jeremiahpasion56@gmail.com>
2024-08-24 23:07:42 +0800

    update authlayout

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,9 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                <NavLink href={route('quizzes.index')} active={route().current('quizzes.index')}>
+                                    Quiz
+                                </NavLink>
                             </div>
                         </div>
 
@@ -94,6 +97,9 @@ export default function Authenticated({ user, header, children }) {
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink href={route('dashboard')} active={route().current('dashboard')}>
                             Dashboard
+                        </ResponsiveNavLink>
+                        <ResponsiveNavLink href={route('quizzes.index')} active={route().current('quizzes.index')}>
+                            Quiz
                         </ResponsiveNavLink>
                     </div>
 
